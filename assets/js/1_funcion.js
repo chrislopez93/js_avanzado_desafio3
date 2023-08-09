596d01fb7a1f3658867e045775721d968b06d6ab
chrislopez93 <sinretel@gmail.com>
2023-08-08 23:11:21 -0400

    Resolución ejercicio 1 desafío

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -3,7 +3,8 @@
 //     return a+b+c
 //     }
 
-var example = function(a,b,c){
+example = function(a,b,c){
     return a + b + c
-}
+};
 
+result = example(3, 4, 5);
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,4 @@
+// Resolución del ejercicio 2 del desafío
+// let suma = function(a, b){
+//     return a + b
+//     }
